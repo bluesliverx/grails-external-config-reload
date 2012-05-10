099efc91f764859f3b78afa613609964ba32028f
Brian Saville <bsaville@adaptivecomputing.com>
2012-05-10 17:17:07 -0600

    Updated version for 1.2.0 release

--- ExternalConfigReloadGrailsPlugin.groovy
+++ ExternalConfigReloadGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import grails.util.Environment;
 
 class ExternalConfigReloadGrailsPlugin {
     // the plugin version
-    def version = "1.1-SNAPSHOT"
+    def version = "1.2-SNAPSHOT"
     
 	// the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
