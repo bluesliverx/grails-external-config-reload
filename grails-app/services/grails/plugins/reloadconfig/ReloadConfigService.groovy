8d5a212545a2d0929985f1f92f8ea67275d6e0e8
Erez Tourjeman <erez.tourjeman@dotplot.com>
2013-07-11 21:13:17 +0200

    Fix #17 - Support .properties config files update

--- grails-app/services/grails/plugins/reloadconfig/ReloadConfigService.groovy
+++ grails-app/services/grails/plugins/reloadconfig/ReloadConfigService.groovy
@@ -42,8 +42,20 @@ class ReloadConfigService {
 			log.debug("Checking external config file location ${configFile} for changes since ${lastTimeChecked}...")
 			if (configFile.exists() && configFile.lastModified()>lastTimeChecked.time) {
 				log.info("Detected changed configuration in ${configFile.name}, reloading configuration")
-				if (automerge)
-					grailsApplication.config.merge(new ConfigSlurper(Environment.getCurrent().getName()).parse(configFile.text))
+				if (automerge) {
+					ConfigSlurper configSlurper = new ConfigSlurper(Environment.getCurrent().getName())
+					ConfigObject updatedConfig
+					if (fileName?.toLowerCase()?.endsWith(".properties")) {
+						def props = new Properties()
+						configFile.withInputStream { stream ->
+							props.load(stream)
+						}
+						updatedConfig = configSlurper.parse(props)
+					} else {
+						updatedConfig = configSlurper.parse(configFile.text)
+					}
+					grailsApplication.config.merge(updatedConfig)
+				}
 				changed << configFile
 			}
 		}
@@ -77,3 +89,4 @@ class ReloadConfigService {
 		notifyPlugins();
 	}
 }
+
